9fa2b4e5548a65fcb09e5fc05ac45e0992bb8ccb
nana-utushi <aikamaolove@gmail.com>
2025-12-04 00:41:05 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://matsunami2:**********@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority&appName=test";
+const uri = "";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
