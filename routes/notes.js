8305a8a69193c31c9c76ab8d9826a82c3bf23028
nana-utushi <79406855+nana-utushi@users.noreply.github.com>
2025-12-04 00:17:39 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://matsunami2:**********@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
